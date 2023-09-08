8e0a4272cb9f26489049a337bda921564b354b2c
PLEASANT <pleasantview076@gmail.com>
2023-09-08 15:21:27 -0700

    print not e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Print all the letters except q and e
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+if (letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
+letter++;
+}
+putchar('\n');
+return (0);
+}

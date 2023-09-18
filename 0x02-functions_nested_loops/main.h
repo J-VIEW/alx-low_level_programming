ef44466af4c692b34943573fd9320848a4789ae0
PLEASANT <pleasantview076@gmail.com>
2023-09-18 11:53:13 -0700

    corrections

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 
 /* Function prototype for print_alphabet function */
 void print_alphabet(void);
+/* Function prototype for print_alphabet_x10 function */
+void print_alphabet_x10(void);
 
 /* _putchar function prototype */
 int _putchar(char c);

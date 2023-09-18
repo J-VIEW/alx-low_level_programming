02b18579c011f11e6bdeab90275389d114b2645f
PLEASANT <pleasantview076@gmail.com>
2023-09-18 12:02:55 -0700

    corrections

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,9 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 /* Function prototype for _islower function */
 int _islower(int c);
+/* Function prototype for _isalpha function */
+int _isalpha(int c);
+
 
 /* _putchar function prototype */
 int _putchar(char c);
